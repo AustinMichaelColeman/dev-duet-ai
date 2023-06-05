fb0e081f9f18047d3c23ac77c429531e122012a3
Austin Coleman <AustinMichaelColeman@gmail.com>
2023-06-05 03:56:50 -0700

    Try redirecting to oauth

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -1,21 +1,24 @@
+import generateRandomString from "@/utils/generateRandomString";
+
 export default function auth(req, res) {
   try {
     const { response_type, client_id, scope, redirect_uri } = req.query;
-    const { GITHUB_APP_INSTALLATION_URL } = process.env;
+    const { GITHUB_OAUTH_AUTHORIZE_URL } = process.env;
 
     console.log("auth req.query", req.query);
     console.log("auth req.body", req.body);
+
+    const state = generateRandomString(16);
 
     const params = new URLSearchParams({
       client_id,
       redirect_uri,
       response_type,
       scope,
+      state,
     });
 
-    // need to check if the app has been installed already or not here
-
-    res.redirect(`${GITHUB_APP_INSTALLATION_URL}?${params.toString()}`);
+    res.redirect(`${GITHUB_OAUTH_AUTHORIZE_URL}?${params.toString()}`);
   } catch (error) {
     console.error(
       "Auth Request Error:",
