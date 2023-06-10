e543484bc48b21d783e8af7ce702ef51bbaad8fa
Austin Coleman <austinmichaelcoleman@gmail.com>
2023-06-09 23:43:35 -0700

    Improve error handling and add comments in aiplugin.js

--- src/pages/api/aiplugin.js
+++ src/pages/api/aiplugin.js
@@ -2,7 +2,14 @@ export default async function aiplugin(req, res) {
   const { SERVER_URL, CONTACT_EMAIL, OPENAI_VERIFICATION_TOKEN, PLUGIN_NAME } =
     process.env;
 
+  // Validate environment variables
+  if (!SERVER_URL || !CONTACT_EMAIL || !OPENAI_VERIFICATION_TOKEN || !PLUGIN_NAME) {
+    res.status(500).send("Missing required environment variables");
+    return;
+  }
+
   try {
+    // Set up the configuration for the AI plugin
     const config = {
       schema_version: "v1",
       name_for_model: PLUGIN_NAME,
@@ -27,9 +34,12 @@ export default async function aiplugin(req, res) {
         url: `${SERVER_URL}/openapi.yaml`,
       },
     };
+
+    // Send the configuration as a JSON response
     res.json(config);
   } catch (err) {
+    // Log the error and send a more specific error message
     console.error(err);
-    res.status(500).send("Configuration error");
+    res.status(500).send(`Configuration error: ${err.message}`);
   }
 }
